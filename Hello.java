b866bcafe49254d1f0efaa868e8e54e6e61fac2a
cliffadcj-wq <cliffad.cj@gmail.com>
2026-05-07 11:26:48 +0100

    Hello Java program

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, I am learning Java");
+		System.out.print("Oh wow, Learning Java is fun. ");
+		System.out.print("Hurray,I am Happy");
+		System.out.printf("Java was created in %d,1992");
+		System.out.printf("Java was made free for %.2f dollar",'$',0.0000);
+		System.out.print(Do you Java%c %b",'?',true);
+		System.out.printf("%s","Yes learning java is fun")
+		
+	} 
+	
+
+}
+
