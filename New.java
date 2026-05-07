5b336a9606cf92e579e47628b68183dd90cf5432
cliffadcj-wq <cliffad.cj@gmail.com>
2026-05-07 16:21:06 +0100

    primitivedatatype and escapechartcer lesson

---
+++ New.java
@@ -0,0 +1,7 @@
+public class EscapeCharacter{
+	public static void main(String[]args){
+		System.out.print("When you need to use the escape characters");
+		System.out.print("Use it when you want to manipulate your cursor");
+		System.out.print("Use \\n when you want a newline");
+	}
+}
---
+++ PrimitiveDataType.java
@@ -0,0 +1,31 @@
+public class PrimitiveDataType{
+	public static void main(String [] args) {
+		byte myAge = 30;
+		System.out.printf("Miss mercy is %d years old\n",myAge);
+		
+		short quantity = 20000;
+		System.out.printf("The quantity of phone ordered is %, d%n",quantity);
+		
+		int nigeriaPopulation = 29848848;
+		System.out.printf("The population of Nigeria is %,d%n",nigeriaPopulation);
+		
+		long worldPopulation = 47474778478474874L;
+		System.out.printf("The world's population is %,d%n",worldPopulation);
+		
+		float price = 577858.84747F;
+		System.out.printf("The price of each iphone per unit is %,.2f%n",price);
+		
+		double myBalance = 746748747747474.8474747;
+		System.out.printf("My account balance is %c%,.2f",'$',myBalance);
+		
+		char symbol = '%';
+		System.out.printf("There is an increment in the world's population by 20.5%c%n",symbol);
+		
+		boolean isJavaFun = true;
+		System.out.printf("Do you love Java? %b",isJavaFun);
+	}
+
+
+
+
+}
